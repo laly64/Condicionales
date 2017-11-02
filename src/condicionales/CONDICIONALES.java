262c6b78fd8456051259d26dd0fc3c3e4f972f74
laly64 <avalos53laly@gmail.com>
2017-11-02 19:43:46 -0300

    Operador de comparacion > en el if y else  anidados

--- src/condicionales/CONDICIONALES.java
+++ src/condicionales/CONDICIONALES.java
@@ -27,7 +27,15 @@ public class CONDICIONALES {
         }
         else //Si no se cumple la condicion se ejecuta esto
         {
-            
+           JOptionPane.showMessageDialog(null, "Son diferentes");
+            if (numero1 > numero2) 
+            {
+              JOptionPane.showMessageDialog(null, "Numero"+numero1+" es mayor");   
+            }
+            else
+            {
+             JOptionPane.showMessageDialog(null, "Numero"+numero2+" es mayor");   
+            }
         }
     }
     

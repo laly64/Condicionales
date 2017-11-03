f11425e40c60ef03f71b9e4183fe7304b1a7377c
laly64 <avalos53laly@gmail.com>
2017-11-03 20:29:33 -0300

    Se a realizado la clase condicionales switch

---
+++ src/condicionales/SwitchNotas.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionales;
+import javax.swing.JOptionPane;
+import java.lang.Math; //paquete para funciones matamaticas
+
+/**
+ *
+ * @author LAB02
+ */
+public class SwitchNotas 
+{
+     public static void main(String[] args)
+     {
+         float nota = Float.parseFloat(JOptionPane.showInputDialog("Ingrese una nota"));
+        //El metodo round es para redondear un decimal 
+         switch(Math.round(nota))
+         {
+             case 7: //Caso 1 - si el valor es 7
+            JOptionPane.showMessageDialog(null, "Notable");
+             break; 
+              case 6: //Caso 2 - si el valor es 6
+            JOptionPane.showMessageDialog(null, "bien");
+             break;
+               case 5: //Caso 3 - si el valor es 5
+            JOptionPane.showMessageDialog(null, "Suficiente");
+             break; 
+                case 4: //Caso 4 - si el valor es 4
+            JOptionPane.showMessageDialog(null, "suficiente");
+             break;
+                default:
+            JOptionPane.showMessageDialog(null, "Insuficiente");
+             break; 
+             
+            
+         }
+         
+     }
+    
+}

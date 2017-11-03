7d3560f0a3ed108fcea7a2ceb2d6139ef534d3e2
laly64 <avalos53laly@gmail.com>
2017-11-02 21:07:10 -0300

    se a realizado el ejercicio de notas aprobo, reprobo

--- src/condicionales/calificaciones.java
+++ src/condicionales/calificaciones.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package condicionales;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author LAB02
  */
-public class calificaciones {
+public class calificaciones 
+        {
+          public static void main(String[] args)
+{
+     float nota1, nota2, nota3, promedio;
+      
+      nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la primer nota"));
+      nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la segunda nota"));
+      nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la tercera nota"));
+      
+      promedio= (nota1 + nota2 + nota3) /3;
+      
+        if (promedio>=4) //Aqui va una condicion 
+            
+        {
+         //Si la condicion se cumple se ejecuta este codigo 
+         JOptionPane.showMessageDialog(null, "Aprobo");
+        }
+        else //Si no se cumple la condicion se ejecuta esto
+        {
+           JOptionPane.showMessageDialog(null, "Reprobo");
+             
+            }
+            
+        }
+    }
     
-}
+
+   
+
